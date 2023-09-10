d542df52615ab2939a111ae978762cd21dfc6d85
Abhishek Vijayakumar <abhishekwhizzer@gmail.com>
2023-09-10 11:06:15 -0500

    added debounce implementation

--- app.js
+++ app.js
@@ -3,6 +3,21 @@ const defaultText = document.getElementById("default");
 const debounceText = document.getElementById("debounce");
 const throttleText = document.getElementById("throttle");
 
+const debounce = (cb, delay) => {
+  let timeOut;
+  return (...args) => {
+    clearTimeout(timeOut);
+    timeOut = setTimeout(() => {
+      cb(...args);
+    }, delay);
+  };
+};
+
+const updateDebounceText = debounce((text) => {
+  debounceText.textContent = text;
+}, 2000);
+
 inputText.addEventListener("input", (e) => {
   defaultText.textContent = e.target.value;
+  updateDebounceText(e.target.value);
 });
